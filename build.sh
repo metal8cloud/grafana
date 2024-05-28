838439bd9070946706ca2adfe5cbf7f04b670156
yuli <stremovsky@gmail.com>
2024-05-28 19:16:49 +0300

    use new repository

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 docker build -t grafana .
-docker tag grafana 192.168.64.3:32000/grafana
-docker push 192.168.64.3:32000/grafana
+#docker tag grafana 192.168.64.3:32000/grafana
+#docker push 192.168.64.3:32000/grafana
+docker tag grafana registry.metal8.cloud/grafana
+docker push registry.metal8.cloud/grafana
 
 microk8s kubectl rollout restart -n observability deployment/kube-prom-stack-grafana
